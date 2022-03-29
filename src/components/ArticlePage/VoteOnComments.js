fbc5eee3586046b0f891a5e0836da767513b4377
MarshmallowFlump <lfhlord@gmail.com>
2022-03-29 02:42:39 +0100

    add comment voting functionality on individual article pages

--- src/components/ArticlePage/VoteOnComments.js
+++ src/components/ArticlePage/VoteOnComments.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { patchArticleCommentByID } from '../../utils/api';
 
-const VoteOnComments = () => {
+const VoteOnComments = (props) => {
+
+    const { votes, comment_id } = props;
+    
+    const [ commentVotes, setCommentVotes ] = useState(0);
+    const [ err, setErr ] = useState(null);
+
+    useEffect(() => {
+        setCommentVotes(votes);
+    }, []);
+
+    const handleUpVoteClick = () => {
+        setCommentVotes((currCount) => currCount +1);
+        setErr(null);
+        patchArticleCommentByID(comment_id, 1)
+        .catch((err) => {
+            setCommentVotes((currCount) => currCount -1);
+            setErr('Something went wrong, please try again.');
+        });
+    };
+
+    const handleDownVoteClick = () => {
+        setCommentVotes((currCount) => currCount -1);
+        setErr(null);
+        patchArticleCommentByID(comment_id, -1)
+        .catch((err) => {
+            setCommentVotes((currCount) => currCount +1);
+            setErr('Something went wrong, please try again.');
+        });
+    };
+
+    if (err) return <p>{err}</p>;
+
     return (
-        <div>
-            <button className='commentUpvote'>
+        <div className='voteOnComment'>
+            <button className='commentUpvote' onClick={handleUpVoteClick}>
                 👍
             </button>
 
             <h2 className='commentCount'>
-                2
+                {commentVotes}
             </h2>
-            <button className='commentDownvote'>
+            <button className='commentDownvote' onClick={handleDownVoteClick}>
                 👎
             </button>
 
