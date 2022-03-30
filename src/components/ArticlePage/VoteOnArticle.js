f10dbb36d491619749cd265ce67b8a448c9c1424
MarshmallowFlump <lfhlord@gmail.com>
2022-03-31 00:35:03 +0100

    restrict voting on article on article page to one up or one down per page load

--- src/components/ArticlePage/VoteOnArticle.js
+++ src/components/ArticlePage/VoteOnArticle.js
@@ -8,6 +8,8 @@ const VoteOnArticle = () => {
 
     const [ votes, setVotes ] = useState(0);
     const [ err, setErr ] = useState(null);
+    const [ upVoted, setUpVoted ] = useState(false);
+    const [ downVoted, setDownVoted ] = useState(false);
 
     useEffect(() => {
         getArticleByID(article_id)
@@ -17,23 +19,56 @@ const VoteOnArticle = () => {
     }, []);
 
     const handleUpVoteClick = () => {
-        setVotes((currCount) => currCount +1);
-        setErr(null);
-        patchArticleVotes(article_id, 1)
-        .catch((err) => {
-            setVotes((currCount) => currCount -1);
-            setErr('Something went wrong, please try again.');
-        });
+        if (upVoted === false) {
+            setUpVoted(true);
+            setDownVoted(false);
+            setVotes((currCount) => currCount +1);
+                setErr(null);
+                patchArticleVotes(article_id, 1).catch((err) => {
+                    setUpVoted(false);
+                    setVotes((currCount) => currCount -1);
+                    setErr('Something went wrong, please try again.');
+            });
+    
+        } else if (upVoted === true && downVoted === false) {
+            alert('You have already upvoted this article.')
+        } else if (upVoted === true && downVoted === true) {
+            setUpVoted(true);
+            setDownVoted(false);
+            setVotes((currCount) => currCount +1);
+            setErr(null);
+            patchArticleVotes(article_id, 1).catch((err) => {   
+                setUpVoted(false);
+                setVotes((currCount) => currCount -1);
+                setErr('Something went wrong, please try again.');
+            });
+        };
     };
 
     const handleDownVoteClick = () => {
-        setVotes((currCount) => currCount -1);
-        setErr(null);
-        patchArticleVotes(article_id, -1)
-        .catch((err) => {
-            setVotes((currCount) => currCount +1);
-            setErr('Something went wrong, please try again.');
-        });
+        if (downVoted === false) {
+            setDownVoted(true);
+            setUpVoted(false);
+            setVotes((currCount) => currCount -1);
+                setErr(null);
+                patchArticleVotes(article_id, -1).catch((err) => {
+                    setDownVoted(false);
+                    setVotes((currCount) => currCount +1);
+                    setErr('Something went wrong, please try again.');
+            });
+        } else if (downVoted === true && upVoted === false) {
+            alert('You have already downvoted this article.')
+        } else if (downVoted === true && upVoted === true) {
+            setUpVoted(false);
+            setDownVoted(true);
+            setVotes((currCount) => currCount -1);
+            setErr(null);
+                patchArticleVotes(article_id, -1).catch((err) => {
+                    setDownVoted(false);
+                    setVotes((currCount) => currCount +1);
+                    setErr('Something went wrong, please try again.');
+            });
+        };
     };
 
     if (err) return <p>{err}</p>;
