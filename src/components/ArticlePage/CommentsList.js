5b751c6b3fdfe64d6210ffb8315837b4d8018e11
MarshmallowFlump <lfhlord@gmail.com>
2022-03-30 17:19:11 +0100

    add delete comment component to comments list component

--- src/components/ArticlePage/CommentsList.js
+++ src/components/ArticlePage/CommentsList.js
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { getArticleComments } from '../../utils/api';
 import VoteOnComments from './VoteOnComments';
+import { UserContext } from '../../contexts/User';
+import DeleteComment from './DeleteComment';
 
 const CommentsList = () => {
 
     const { article_id } = useParams();
+
+    //this will be used to confirm current logged in user in order to post or delete comments under the correct user profile
+    const { loggedInUser } = useContext(UserContext);
+
+    //temporary hard-coded user for development purposes
+    const hardCodedUser = 'jessjelly'
 
     const [ comments, setComments ] = useState([]);
 
@@ -38,6 +46,12 @@ const CommentsList = () => {
                             </p>
                         
                         <VoteOnComments comment_id={comment.comment_id} votes={comment.votes} />
+                        
+                        {hardCodedUser === comment.author && (
+                        
+                        <DeleteComment comment_id={comment.comment_id}/>
+
+                        )}
                         
                         </li>
                     )
