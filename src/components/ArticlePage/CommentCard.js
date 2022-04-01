22099894be5aec8f4642687aed693396a81c1c38
MarshmallowFlump <lfhlord@gmail.com>
2022-04-01 05:37:07 +0100

    refactor comment card to include new dropdown menu for comments

--- src/components/ArticlePage/CommentCard.js
+++ src/components/ArticlePage/CommentCard.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react/cjs/react.development';
+import { getArticleComments } from '../../utils/api';
+import Dropdown from '../ArticlePage/Dropdown';
 import CommentsList from './CommentsList';
 
 const CommentCard = (props) => {
 
     const { comment_count } = props;
+
+    const { article_id } = useParams();
+
+    const [ comments, setComments ] = useState([]);
+
+    const [ order, setOrder ] = useState('desc');
+
+    const [ sort, setSort ] = useState(null);
+
+    useEffect(() => {
+        getArticleComments(article_id, sort, order)
+        .then((res) => {
+            setComments(res);
+        });
+    }, [sort, order]);
+
   
     return (
         <div className='commentCard'>
             <h1>Comments ({comment_count})</h1>
-            <CommentsList />
+            <Dropdown setSort={setSort}  setOrder={setOrder}/>
+            <CommentsList  comments={comments}/>
         </div>
     );
 };
