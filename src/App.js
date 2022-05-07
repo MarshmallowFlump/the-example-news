0355531afe12fd1d5e0a45aae5ef757f15bf5ff3
MarshmallowFlump <lfhlord@gmail.com>
2022-05-07 23:50:42 +0100

    refactor user state and add user profile route

--- src/App.js
+++ src/App.js
@@ -2,36 +2,53 @@ import './App.css';
 import ArticlesPage from './components/ArticlesPage/ArticlesPage';
 import ArticlePage from './components/ArticlePage/ArticlePage'
 import Header from './components/Header';
+import MyProfile from './components/UserProfile/MyProfile.js';
+import MyArticlesPage from './components/MyArticles/MyArticlesPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { UserContext } from './contexts/User'
-import { useState } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react';
+import { getUser } from './utils/api'
 
 function App() {
   
-  const [ user, setUser ] = useState('jess_jelly');
+  const user = 'jessjelly';
+
+  const [ profile, setProfile ] = useState({});
+
+  useEffect(() => {
+    getUser(user)
+    .then((res) => {
+        setProfile(res.user);
+    })
+}, [user]);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
     <BrowserRouter>
+
       <div className="App">
+
         <Header />
+
           <div className='content'>
+
               <Routes>
 
-              <Route path="/" element={<ArticlesPage />} /> 
-              <Route path="/articles" element={<ArticlesPage />} />
-              <Route path="/articles/:article_id" element={<ArticlePage />} />
+                <Route path="/" element={<ArticlesPage profile={profile}/> } /> 
+
+                <Route path="/articles" element={<ArticlesPage profile={profile}/>} />
+
+                <Route path="/articles/:article_id" element={<ArticlePage profile={profile}/>} />
+                
+                <Route path="/users/:username" element={<MyProfile profile={profile}/>}  />
+
+                <Route path="/my-articles/:user_id" element={<MyArticlesPage profile={profile}/> } />
 
               </Routes>  
            
-
           </div>
-        
       
       </div>
 
     </BrowserRouter>
-    </UserContext.Provider>
   );
 }
 
