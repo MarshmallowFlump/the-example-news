edaf4d1307292052ecbfb796abb16133fe682b76
MarshmallowFlump <lfhlord@gmail.com>
2022-05-08 00:16:48 +0100

    add non-functional my articles page

---
+++ src/components/MyArticles/MyArticlesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MyArticlesPage = (props) => {
+
+    return (
+        <div>
+            <h1>My Articles</h1>
+
+            <p>articles will go here</p>
+            
+        </div>
+    );
+};
+
+export default MyArticlesPage;
