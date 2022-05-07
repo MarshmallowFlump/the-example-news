b3a227d734172d7a694557590a05667fe04c7f2d
MarshmallowFlump <lfhlord@gmail.com>
2022-05-08 00:12:27 +0100

    update props and add links to user profile and post new article pages

--- src/components/ArticlesPage/ArticlesNav.js
+++ src/components/ArticlesPage/ArticlesNav.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import Dropdown from './Dropdown';
+import { Link } from 'react-router-dom';
+
 
 const ArticlesNav = (props) => {
 
-    const { setTopic, setSort, setOrder } = props;
+    const { setTopic, setSort, setOrder, profile } = props;
 
     return (
      
         <main className='articlesNav'> 
 
-            <h1 className='topicsHeader'>
+            <h2>
+                
+                Welcome, {profile.name}!
+            
+            </h2>
 
-                Navigation
+            <h1>
+                
+                <Link to={`/users/${profile.username}`}>
+
+                My Profile
+
+                </Link>
+            
+                <Link to={`/z`}>
+
+                    Post New Article
+
+                </Link>
 
             </h1>
 
             <Dropdown 
-                setTopic={setTopic}
-                setSort={setSort}
-                setOrder={setOrder}
+                    setTopic={setTopic}
+                    setSort={setSort}
+                    setOrder={setOrder}
             />
-            
+                
         </main>
     );
 };
