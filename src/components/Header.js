5183d2b70fe078e8255dce4a075c1add6a906096
MarshmallowFlump <lfhlord@gmail.com>
2022-05-18 02:51:54 +0100

    add welcome banner to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
+
+    const { name } = props;
+
     return (
-        <div className='header'>
-        <h1>
-            The Example News
-        </h1>
+        <div className='headerBox'>
+
+            <h1 className='header'>
+
+                The Example News
+        
+            </h1>
+
+            <h4 className='welcome'>
+                
+               Welcome, {name}!
+            
+            </h4>
+
         </div>
+        
     );
 };
 
