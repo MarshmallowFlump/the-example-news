7ca6bda4f59e9a7f9fba736694baa814870aebbd
MarshmallowFlump <lfhlord@gmail.com>
2022-05-20 05:24:06 +0100

    create error 404 page

---
+++ src/components/Error404.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from './reuseable/Home';
+
+const Error404 = () => {
+    return (
+        <main>
+
+            <Home />
+
+            <h1>
+                
+                Error 404: Page not found.
+                    
+            </h1>
+
+        </main>
+        
+    );
+};
+
+export default Error404;
